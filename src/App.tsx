76a52bfe2fc291905b7941afd4a7c76e42c877a8
Viktoria <va.2816275@yandex.com>
2025-10-08 13:53:57 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{maxWidth: 680, margin: '48px auto', padding: 24}}>
+      <h1>Todo List</h1>
+      <p style={{opacity: 0.7, marginTop:8 }}>
+        Форма и список
+      </p>
+    </div>
+  );
+}
+
+export default App;
